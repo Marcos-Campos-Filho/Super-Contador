f208794790b82bf69b6c4d3d92d81fc3a32be912
Marcos Antônio Campos Filho <marcoscampos96@hotmail.com>
2021-12-06 14:55:19 -0300

    uptade script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+function contar() {
+    let ini = document.getElementById('txtin')
+    let fim = document.getElementById('txtfin')
+    let passo = document.getElementById('txtpas')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = "Impossível Contar!"
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            alert('Passo Inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {   //CONTAGEM CRESCENTE
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {      //CONTAGEM DECRESCENTE
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
+}
